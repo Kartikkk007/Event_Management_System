64d364b1583627be623b5ab5456ca2c4d891b911
Kartik Sharma <kartiksharma4418@gmail.com>
2026-01-07 02:07:53 +0530

    minor change to index.js for production purpose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,20 @@
-
 import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-// Fallback to 5000 only for local development
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-  
-    await connectDB();
-
+    await connectDB(); // WAIT for MongoDB
     app.listen(PORT, () => {
-      console.log(`✅ Server running on port ${PORT}`);
+      console.log(` Server running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Server failed to start:", error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
